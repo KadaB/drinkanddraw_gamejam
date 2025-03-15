5290218fef95abf38fda81c2251cae7261bbe211
mfalk <Moritz@falkhf.de>
2025-03-15 11:14:02 +0100

    Smol build.bat update

--- build.bat
+++ build.bat
@@ -1,5 +1,4 @@
 @echo off
-pushd build
-clang -g ../code/main.c -I ../include -I ../include/SDL3 ../lib/Windows/x64/SDL3.lib -o game.exe
+IF NOT EXIST build (mkdir build)
+clang -g code/main.c -I include -I include/SDL3 lib/Windows/x64/SDL3.lib -o build/game.exe
 REM clang -g ../code/main.c -Wl,/SUBSYSTEM:WINDOWS -I ../include -I ../include/SDL3 ../lib/Windows/x64/SDL3.lib -o game.exe
-popd
